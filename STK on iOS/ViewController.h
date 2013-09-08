a2fdd8533eedd2b2bfa438c2194014bd102bf2ee
Ariel Elkin <ariel@arivibes.com>
2013-09-08 14:02:08 +0100

    initial commit, empty xcode project

---
+++ STK on iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  STK on iOS
+//
+//  Created by Ariel Elkin on 08/09/2013.
+//  Copyright (c) 2013 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
