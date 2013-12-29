d402e88f31980e6be36ea2a8dfdb246898e93b06
Ariel Elkin <arielelkin@gmail.com>
2013-12-29 01:10:08 +0100

    Updated ViewController to use SineWave

--- STK on iOS/ViewController.mm
+++ STK on iOS/ViewController.mm
@@ -7,11 +7,14 @@
 //
 
 #import "ViewController.h"
+
 #import "AEAudioController.h"
 #import "AEBlockChannel.h"
 
-@interface ViewController ()
+#import "SineWave.h"
 
+@interface ViewController ()
+@property AEAudioController *audioController;
 @end
 
 @implementation ViewController
@@ -21,7 +24,7 @@
     [super viewDidLoad];
     
     //Create an AEAudioController to setup our realtime audio session:
-    AEAudioController *audioController = [[AEAudioController alloc] initWithAudioDescription:
+    self.audioController = [[AEAudioController alloc] initWithAudioDescription:
                                           [AEAudioController nonInterleavedFloatStereoAudioDescription]
                                                                                 inputEnabled:NO
                                           ];
@@ -29,12 +32,17 @@
     NSError *errorAudioSetup = NULL;
     
     //Start the audio session:
-    BOOL result = [audioController start:&errorAudioSetup];
+    BOOL result = [self.audioController start:&errorAudioSetup];
     if ( !result ) NSLog(@"Error starting audio engine: %@", errorAudioSetup.localizedDescription);
     
     
-    //We'll be using this channel to listen to the sound from the STK classes:
-    AEBlockChannel *blockChannel = [AEBlockChannel channelWithBlock:^(const AudioTimeStamp *time, UInt32 frames, AudioBufferList *audio) {
+    //Initialise a SineWave from the STK:
+    stk::SineWave *mySineWave = new stk::SineWave();
+    mySineWave->setFrequency(400);
+    
+    
+    //We'll be using this channel to listen to the sound of the SineWave:
+    AEBlockChannel *myBlockChannel = [AEBlockChannel channelWithBlock:^(const AudioTimeStamp *time, UInt32 frames, AudioBufferList *audio) {
         
         //The STK classes compute and output one sample at a time,
         //place them in the frames of the buffer:
@@ -42,19 +50,14 @@
         for ( int i=0; i<frames; i++ ) {
             
             ((float*)audio->mBuffers[0].mData)[i] =
-            ((float*)audio->mBuffers[1].mData)[i] = 0;
+            ((float*)audio->mBuffers[1].mData)[i] = mySineWave->tick();
         }
 
     }];
     
-    [audioController addChannels:@[blockChannel]];
+    [self.audioController addChannels:@[myBlockChannel]];
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
